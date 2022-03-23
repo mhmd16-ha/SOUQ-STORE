4be8ac46d820beb3d4f2f298faf2d7b11c49165f
mhmd16-ha <https://github.com/mhmd16-ha/Todo_List.git>
2022-03-23 14:01:44 +0200

    Add project files.

---
+++ SOUQ STORE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SOUQ_STORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
