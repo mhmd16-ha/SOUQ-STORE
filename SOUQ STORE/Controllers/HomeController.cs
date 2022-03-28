ad38789c382fd1c433ca57901af355982448135c
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-03-28 17:05:06 +0200

    Removed old DB and some dummy code and added new one - project is working now

--- SOUQ STORE/Controllers/HomeController.cs
+++ SOUQ STORE/Controllers/HomeController.cs
@@ -1,69 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using SOUQ_STORE.Models;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using SOUQ_STORE.Data;
 
 namespace SOUQ_STORE.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly SOUQContext _context;
+        private readonly SOUQContext _context;
 
-        //public HomeController(SOUQContext context)
+        public HomeController(SOUQContext context) => _context = context;
+
+        public async Task<IActionResult> Index() => View(await _context.Products.ToListAsync());
+
+        //public IActionResult Product(int Id)
+        //{ 
+        //    SOUQContext _context = new SOUQContext();
+        //    var result = _context.Products.Find(Id);
+        //    return View(result);
+        //}
+        //public IActionResult Products(int id)
         //{
-        //    _context = context;
+        //    SOUQContext _context = new SOUQContext();
+        //    Category c=_context.Categories.Find(id);
+        //    ViewBag.Cat = c?.Name;
+        //    var Result = _context.Products.Where(x => x.CatId == id).OrderByDescending(x => x.Name).ToList();
+        //    return View(Result);
+        //}
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public IActionResult SaveReviews(Review review)
+        //{
+        //    SOUQContext _context = new SOUQContext();
+        //    _context.Add(review);
+        //    _context.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+        //public IActionResult Cart()
+        //{
+        //    return View();
+        //}
+        //public IActionResult Categories()
+        //{
+        //    SOUQContext _context = new SOUQContext();
+        //   var Result=_context.Categories.ToList();
+        //    return View(Result);
         //}
 
-        public IActionResult Index()
-        {
-            SOUQContext _context = new SOUQContext();
-             _context.Categories.ToList();
-             _context.Products.ToList();
-           
-
-            return View(_context);
-        }
-        public IActionResult Product(int Id)
-        { 
-            SOUQContext _context = new SOUQContext();
-            var result = _context.Products.Find(Id);
-            return View(result);
-        }
-        public IActionResult Products(int id)
-        {
-            SOUQContext _context = new SOUQContext();
-            Category c=_context.Categories.Find(id);
-            ViewBag.Cat = c?.Name;
-            var Result = _context.Products.Where(x => x.CatId == id).OrderByDescending(x => x.Name).ToList();
-            return View(Result);
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult SaveReviews(Review review)
-        {
-            SOUQContext _context = new SOUQContext();
-            _context.Add(review);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-        public IActionResult Cart()
-        {
-            return View();
-        }
-        public IActionResult Categories()
-        {
-            SOUQContext _context = new SOUQContext();
-           var Result=_context.Categories.ToList();
-            return View(Result);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
---
+++ SOUQ STORE/Data/Migrations/20220328145341_RemovedSQLServerDbAndAddedSQLite.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SOUQ_STORE.Data.Migrations
+{
+    public partial class RemovedSQLServerDbAndAddedSQLite : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    Photo = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    DataFilter = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true),
+                    Subject = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18, 0)", nullable: true),
+                    CatId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Photo = table.Column<string>(type: "TEXT", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Categories",
+                        column: x => x.CatId,
+                        principalTable: "Categories",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Card",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserId = table.Column<string>(type: "TEXT", nullable: true),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: true),
+                    Qty = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Card", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Card_Product",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Card_ProductId",
+                table: "Card",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CatId",
+                table: "Product",
+                column: "CatId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Card");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
